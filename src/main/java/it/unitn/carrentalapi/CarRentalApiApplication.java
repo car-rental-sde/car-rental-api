3dd3f95fff099448fa5cb08cd9842628d17c7592
Mateusz Chudek <m.chudek@kaft.pl>
2024-12-25 14:07:54 +0100

    Initial commit

---
+++ src/main/java/it/unitn/carrentalapi/CarRentalApiApplication.java
@@ -0,0 +1,13 @@
+package it.unitn.carrentalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarRentalApiApplication.class, args);
+    }
+
+}
