10a4bf5f522df198484e9bd554a5fa4e4e82017f
Mateusz Chudek <m.chudek@kaft.pl>
2025-01-03 19:58:50 +0100

    Add JacksonConfig

--- src/main/java/it/unitn/carrentalapi/config/JacksonConfig.java
+++ src/main/java/it/unitn/carrentalapi/config/JacksonConfig.java
@@ -1,17 +1,18 @@
 package it.unitn.carrentalapi.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.openapitools.jackson.nullable.JsonNullableModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class JacksonConfig {
-
     @Bean
     public ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JsonNullableModule());
+        objectMapper.registerModule(new JavaTimeModule());
         return objectMapper;
     }
 }
